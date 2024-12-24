f229d93ff96f887e345c37ac83e58c65689e7211
arafat-alim <arafat97alim@gmail.com>
2024-12-25 00:59:53 +0530

    ✔️ `Restful-apis-sql`: UserAuth - Migration FIle updated.

--- src/migrations/20241224190200_create_users_auth_table.js
+++ src/migrations/20241224190200_create_users_auth_table.js
@@ -7,12 +7,12 @@ exports.up = function (knex) {
     table.increments("id").primary();
     table.string("email", 50).notNullable().unique();
     table.string("name", 50).notNullable();
-    table.string("password", 20).notNullable();
+    table.string("password", 100).notNullable();
     table.boolean("verified").defaultTo(false);
     table.string("verificationCode", 10);
-    table.string("verificationCodeValidation", 50);
+    table.string("verificationCodeValidation", 100);
     table.string("forgotPasswordCode", 10);
-    table.string("forgotPasswordCodeValidation", 50);
+    table.string("forgotPasswordCodeValidation", 100);
     table.boolean("deletedUser").defaultTo(false);
     table.timestamps(true, true); // Adds created_at and updated_at columns
   });
