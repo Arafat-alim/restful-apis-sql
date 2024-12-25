d1c452d905ffe59dce2c89b95d2df0a7256b2312
arafat-alim <arafat97alim@gmail.com>
2024-12-26 00:57:50 +0530

    ✔️ `Restful-apis-sql`: UserAuth - Production database set up completed.

--- knexfile.js
+++ knexfile.js
@@ -9,10 +9,36 @@ module.exports = {
       password: process.env.POSTGRES_DB_PASSWORD,
       database: process.env.POSTGRES_DB_NAME,
       port: process.env.POSTGRES_DB_PORT,
+      ssl: {
+        rejectUnauthorized: false,
+      },
     },
     migrations: {
       directory: "./src/migrations",
     },
+    seeds: {
+      directory: "./seeds",
+    },
     useNullAsDefault: true,
+  },
+
+  production: {
+    client: "pg", // Replace with your production database client
+    connection: {
+      host: process.env.PRODUCTION_DB_HOST,
+      user: process.env.PRODUCTION_DB_USERNAME,
+      password: process.env.PRODUCTION_DB_PASSWORD,
+      database: process.env.PRODUCTION_DB_NAME,
+      port: process.env.PRODUCTION_DB_PORT,
+      ssl: {
+        rejectUnauthorized: false, // Adjust based on your certificate setup
+      },
+    },
+    migrations: {
+      directory: "./src/migrations",
+    },
+    seeds: {
+      directory: "./src/seeds",
+    },
   },
 };
