eec86038cf953784bd2bedd96da5c26a2c4c789d
arafat-alim <arafat97alim@gmail.com>
2024-12-25 18:13:13 +0530

    ✔️ `Restful-apis-sql`: UserAuth - Updated the users table, length capacity of `sendVerificationCode` and `forgotPasswordCode` increased.

---
+++ src/migrations/20241225123401_update_users_auth_table_columns_capacity.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.alterTable("users", (table) => {
+    table.string("verificationCode", 100).alter();
+    table.string("forgotPasswordCode", 100).alter();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.alterTable("users", (table) => {
+    table.string("verificationCode", 10).alter();
+    table.string("forgotPasswordCode", 10).alter();
+  });
+};
